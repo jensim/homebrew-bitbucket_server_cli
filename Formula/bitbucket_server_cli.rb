d283ee2a9952c0acf64b9818cca256b305b479ea
Jens Brimfors <jens.brimfors@nordnet.se>
2020-03-29 21:10:32 +0200

    Update to version 0.2.2

--- Formula/bitbucket_server_cli.rb
+++ Formula/bitbucket_server_cli.rb
@@ -3,7 +3,7 @@ class BitbucketServerCli < Formula
   homepage "https://github.com/jensim/bitbucket_server_cli"
   url "https://github.com/jensim/bitbucket_server_cli/releases/download/0.2.2/bitbucket_server_cli-mac.tar.gz"
   sha256 "ca901ef2ab80e6cfeb36792a8b2371cae673c097d5a693631305ba93c6621bee"
-  version "0.2.0"
+  version "0.2.2"
 
   def install
     bin.install "bitbucket_server_cli"
