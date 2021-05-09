e4498dd1ec39e13270fb9b9e3bc28b2f281e0dba
Jens Brimfors <jens.brimfors@nordnet.se>
2021-05-09 16:03:37 +0200

    Update to version 0.4.0

--- Formula/bitbucket_server_cli.rb
+++ Formula/bitbucket_server_cli.rb
@@ -1,9 +1,9 @@
 class BitbucketServerCli < Formula
   desc "Clone and update entire projects from BitBucket server, has interactive and non-interactive mode."
   homepage "https://github.com/jensim/bitbucket_server_cli"
-  url "https://github.com/jensim/bitbucket_server_cli/releases/download/0.3.15/bitbucket_server_cli-osx.tgz"
-  sha256 "361b60599dd717004b18a5c5f90c1c02a59bfa6e9bc3361751cc0ffa785ec4e6"
-  version "0.3.15"
+  url "https://github.com/jensim/bitbucket_server_cli/releases/download/0.4.0/bitbucket_server_cli-macOS-latest.tgz"
+  sha256 "bff39cfbfb8f0b0e328038943b3e1fe2aa2c5dce0027335176cd728c0ecfcea8"
+  version "0.4.0"
 
   def install
     bin.install "bitbucket_server_cli"
